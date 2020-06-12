cf3cb0450d8e893b003338eed096242fd0e562c2
Rinata Baygulova <rinulya0912@gmail.com>
2020-06-12 14:44:10 +0300

    feat: добавить js для формы и кнопок

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close-btn');
+const formElement = popup.querySelector('.form');
+const nameInput = formElement.querySelector('.form__item_el_name');
+const jobInput = formElement.querySelector('.form__item_el_job');
+const profileInfo = document.querySelector('.profile__info');
+const profileName = profileInfo.querySelector('.profile__name');
+const profileJob = profileInfo.querySelector('.profile__job');
+const profileEditButton = profileInfo.querySelector('.profile__btn_type_edit');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  const name = nameInput.value;
+  const job = jobInput.value;
+
+  profileName.textContent = name;
+  profileJob.textContent = job;
+
+  closePopup();
+}
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopup () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+popupCloseButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+
